cfbe167b0218b70bc7d9a49cb8ffc509377d038b
ChristosK17 <christoskiokakis@gmail.com>
2024-01-08 22:19:46 +0200

    Fixed update method, Implemented test methods to measure performance

--- db_handler.py
+++ db_handler.py
@@ -18,44 +18,108 @@ class MuseumDatabase:
         self.cursor.execute(query)
         self.conn.commit()
 
+    @timeIt
     def readAll(self, tableName):
         query = f"""SELECT * FROM {tableName}"""
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
+    @timeIt
     def readBy(self, tableName, attributes):
         conditions = ' AND '.join([f'{key} = "{value}"' for key, value in attributes.items()])
         query = f"""SELECT * FROM {tableName} WHERE {conditions}"""
         self.cursor.execute(query)
         return self.cursor.fetchall()
     
+    @timeIt
     def updateBy(self, tableName, toUpdate, conditions):
         toUpdate = ' AND '.join([f'{key} = "{value}"' for key, value in toUpdate.items()])
         conditions = ' AND '.join([f'{key} = "{value}"' for key, value in conditions.items()])
-        query = f"""SELECT * FROM {tableName} SET {toUpdate} WHERE {conditions}"""
+        query = f"""UPDATE {tableName} SET {toUpdate} WHERE {conditions}"""
+        print("UPDATE QUERY: "+query)
         self.cursor.execute(query)
         return self.cursor.fetchall()
-        
+    
+    @timeIt
     def deleteAll(self, tableName):
         query = f"""DELETE FROM {tableName}"""
         self.cursor.execute(query)
         self.conn.commit()
 
+    @timeIt
     def deleteBy(self, tableName, attributes):
         conditions = ' AND '.join([f'{key} = "{value}"' for key, value in attributes.items()])
         query = f"""DELETE FROM {tableName} WHERE {conditions}"""
         self.cursor.execute(query)
         self.conn.commit()
 
+    def generateToTestRelations(self):
+        query_list = """
+                insert into VISITS (id, date, category) values (16, '11/23/2023', 'Adult');
+                insert into VISITS (id, date, category) values (86, '6/27/2023', 'Elderly');
+                insert into VISITS (id, date, category) values (36, '8/28/2023', 'Disabled');
+                insert into VISITS (id, date, category) values (77, '2/23/2023', 'Adult');
+                insert into VISITS (id, date, category) values (46, '12/18/2023', 'Adult');
+                insert into VISIT_TICKET (id, category, visitId) values (27, 'Adult', 16);
+                insert into VISIT_TICKET (id, category, visitId) values (74, 'Elderly', 86);
+                insert into VISIT_TICKET (id, category, visitId) values (21, 'Disabled', 36);
+                insert into VISIT_TICKET (id, category, visitId) values (68, 'Adult', 77);
+                insert into VISIT_TICKET (id, category, visitId) values (34, 'Adult', 46);
+                """.split(";")
+        for query in query_list:
+            self.cursor.execute(query+";")
+            self.conn.commit()
+    
+    @timeIt
+    def generateToTestPerformance(self, amount):
+        from faker import Faker
+        import random
+        fake = Faker("el_GR")
+        for i in range(amount):
+            query = f"INSERT INTO PERSON (id, firstName, lastName, phone, email) VALUES ({i}, '{str(fake.first_name())}', '{str(fake.last_name())}', '{str(''.join((random.choice('0123456789') for i in range(10))))}', '{str(''.join((random.choice('abcdxyzpqr') for i in range(15))))}')"
+            self.cursor.execute(query)
+            self.conn.commit()
+
 
 if __name__ == "__main__":
     museum_db = MuseumDatabase('ArcheologicalMuseum.db', "schema.sql")
     
-    museum_db.create(tableName="EMPLOEEY", attributes={"id":2314,"specialty":"test3" })
+    #museum_db.create(tableName="EMPLOEEY", attributes={"id":2314,"specialty":"test3" })
     
-    result = museum_db.readAll("EMPLOEEY")
+    museum_db.deleteAll("VISITS")
+    museum_db.deleteAll("VISIT_TICKET")
+
+    museum_db.generateToTestRelations()
     
+    result = museum_db.readAll("VISITS")
     print(result)
+
+    result = museum_db.readAll("VISIT_TICKET")
+    print(result)
+
+    result = museum_db.readBy("VISIT_TICKET", attributes={"id":27})
+    print(result)
+
+    museum_db.updateBy("VISIT_TICKET", toUpdate={"category":"TEST"}, conditions={"id":27})
+
+    result = museum_db.readBy("VISIT_TICKET", attributes={"id":27})
+    print(result)
+
+    museum_db.deleteBy("VISIT_TICKET", attributes={"id":27})
+
+    result = museum_db.readBy("VISIT_TICKET", attributes={"id":27})
+    print(result)
+
+    for i in [1, 10, 100, 1000, 10000]:
+        print(f"\n\n FOR {i} INSERTIONS\n")
+
+        museum_db.deleteAll("PERSON")
+
+        museum_db.generateToTestPerformance(i)
+
+        result = museum_db.readAll("PERSON")
+        #print(result)
+
 
     museum_db.conn.close()
     
